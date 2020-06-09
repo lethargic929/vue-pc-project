bde3cdff1c39d848bdc03a8e86e6fb64b867bb68
chengfangfang <429815654@qq.com>
2020-06-09 23:45:52 +0800

    03 首页基本布局

--- src/router/index.js
+++ src/router/index.js
@@ -14,14 +14,22 @@ const routes = [
     name: 'home',
     component: () => import('../views/home/Home.vue'),
     children: [
-     
+      { path: '/', component: () => import('../views/home/Welcome.vue') },
+     {path:'/article',component:() => import('../views/home/Article.vue')},
+     {path:'/image',component:() => import('../views/home/Image.vue')},
+     {path:'/publish',component:() => import('../views/home/Publish.vue')},
+     {path:'/comment',component:() => import('../views/home/Comment.vue')},
+     {path:'/fans',component:() => import('../views/home/Fans.vue')},
+     {path:'/setting',component:() => import('../views/home/Setting.vue')},
+      
     ]
   }
   
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  linkExactActiveClass: 'active'
 })
 
 export default router
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,6 +1,40 @@
 <template>
   <div class="home">
-    <h1>首页</h1>
+  <el-container>
+  <el-header>黑马头条</el-header>
+  <el-container>
+    <el-aside width="200px">
+       <el-menu>
+      <el-menu-item index="1">
+        <template slot="title"><i class="el-icon-message"></i><router-link to="/">首页</router-link></template>
+      </el-menu-item>
+      <el-menu-item index="1">
+        <template slot="title"><i class="el-icon-message"></i><router-link to="/article">内容管理</router-link></template>
+      </el-menu-item>
+      <el-menu-item index="1">
+        <template slot="title"><i class="el-icon-message"></i> <router-link to="/image">素材管理</router-link></template>
+      </el-menu-item>
+      <el-menu-item index="1">
+        <template slot="title"><i class="el-icon-message"></i><router-link to="/publish">发布文章</router-link></template>
+      </el-menu-item>
+      <el-menu-item index="1">
+        <template slot="title"><i class="el-icon-message"></i><router-link to="/comment">评论管理</router-link></template>
+      </el-menu-item>
+      <el-menu-item index="1">
+        <template slot="title"><i class="el-icon-message"></i> <router-link to="/fans">粉丝管理</router-link></template>
+      </el-menu-item>
+      <el-menu-item index="1">
+        <template slot="title"><i class="el-icon-message"></i><router-link to="/setting">个人设置</router-link></template>
+      </el-menu-item>
+       </el-menu>
+ 
+    </el-aside>
+    <el-main>
+      <!-- 江苏传智播客科技教育有限公司 -->
+      <router-view></router-view>
+      </el-main>
+  </el-container>
+</el-container>
   </div>
 </template>
 
@@ -12,4 +46,24 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+.el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
 </style>
