3258ee52d8920b68796f2d08e06852909f7cc3fc
chengfangfang <429815654@qq.com>
2020-06-09 12:14:03 +0800

    01-搭建项目

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+    <h1>登录页</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'login'
+
+}
+</script>
+<style lang="less" scoped>
+// .login {
+//  background-image: url(../assets/login_bg.jpg);
+// }
+</style>
